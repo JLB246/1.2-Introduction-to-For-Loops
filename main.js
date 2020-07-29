0ca27ca6a7efb286d9a283a807c9a6c93469c091
JLB246 <65634399+JLB246@users.noreply.github.com>
2020-07-29 15:30:36 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+for (var i = 2; i <= 100; i=i+2){
+  console.log(i);
+}
+for (var i = 100; i >= 2; i=i-2){
+  console.log(i);
+}
+for (var i = 0; i <= 100; i=i+5){
+  console.log(i);
+}
+for(let i = 0; i < 7; i++) {
+  console.log('All code and no play makes Joshua Byrd a great coder');
+}
